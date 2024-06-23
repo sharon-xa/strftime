53fad1083e532a854e7171009888d268ef0ff218
sharon <ali93456@gmail.com>
2024-06-23 17:35:58 +0300

    chore: add a building script

---
+++ buildAndPublish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+check_version_change() {
+  read -p "Have you changed the version number in the package.json file? (yes/no): " response
+
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      echo "Great! Make sure to commit the changes."
+      ;;
+    [nN][oO]|[nN])
+      echo "Please update the version number in the package.json file before proceeding."
+      exit 1
+      ;;
+    *)
+      echo "Invalid response. Please answer yes or no."
+      check_version_change
+      ;;
+  esac
+}
+
+check_version_change
+
+echo "Building the library..."
+tsc 
+
+npm publish
+
+exit 0
