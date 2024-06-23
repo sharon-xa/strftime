56ed752440af8a3fc2a0fc736fe5c51ffafa4be8
sharon <ali93456@gmail.com>
2024-06-23 04:35:36 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,112 @@
+/* 
++----------+
+| STRFTIME |
++----------+
+Author: https://github.com/sharon-xa
+Description: silly strftime function implementation in js without the percentage notation.
+             based off https://strftime.org
+
+*/
+import { DateFormat } from "./types";
+
+// Pads a number with zeros to a specified length.
+// returns The padded number as a string.
+export function pad(originalNum: number, padSize: number = 2): string {
+  const paddedString = Array(padSize).join('0') + originalNum;
+  return paddedString.substring(paddedString.length - padSize);
+};
+
+// Returns the ordinal suffix for a number (e.g., 'st', 'nd', 'rd', 'th').
+export function ord(num: number): string {
+  const stringOfNumber: string = num.toString();
+  const lastDigit: number = parseInt(stringOfNumber[stringOfNumber.length - 1]);
+  const secondLastDigit: number = parseInt(stringOfNumber[stringOfNumber.length - 2]);
+
+  if (secondLastDigit === 1) return 'th';
+
+  else
+    switch (lastDigit) {
+      case 1:
+        return 'st'; // 1st, 21st, 31st, ...
+      case 2:
+        return 'nd'; // 2nd, 22nd, 32nd, ...
+      case 3:
+        return 'rd'; // 3rd, 23rd, 33rd, ...
+      default:
+        return 'th'; // 4th, 5th, 6th, 7th, 8th, 9th, 10th, 11th (for numbers not covered by the above cases)
+    }
+
+};
+
+// Checks if a given value is a valid Date object.
+// Returns true if the value is a valid Date object and represents a valid date; otherwise, false.
+export function isDateValid(date: any): boolean {
+
+  if (date instanceof Date && !isNaN(date.getTime()))
+    return true
+
+  return false
+}
+
+// Extends the Date prototype to format a date and time string based on the specified format.
+// Throws an error if the provided date is invalid.
+export function strftime(date: Date, format = "c"): string {
+
+  if (!isDateValid(date)) {
+    throw new Error('Invalid date provided');
+  }
+
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const formats: DateFormat = {
+    a: days[date.getDay()].substring(0, 3),
+    A: days[date.getDay()],
+    w: date.getDay(),
+    q: pad(date.getDay()),
+    d: pad(date.getDate()),
+    e: date.getDate(),
+    b: month[date.getMonth()].substring(0, 3),
+    B: month[date.getMonth()],
+    m: date.getMonth() + 1,
+    N: pad(date.getMonth() + 1),
+    y: pad(date.getFullYear()),
+    Y: date.getFullYear(),
+    H: date.getHours(),
+    h: pad(date.getHours()),
+    I: pad(date.getHours() % 12 || 12),
+    p: date.getHours() >= 12 ? 'PM' : 'AM',
+    o: ord(date.getDate()),
+    M: date.getMinutes(),
+    i: pad(date.getMinutes()),
+    S: date.getSeconds(),
+    s: pad(date.getSeconds()),
+    f: date.getMilliseconds(),
+    c: date.toDateString() + ' - ' + date.toTimeString(),
+    x: date.toLocaleDateString(),
+    X: date.toLocaleTimeString()
+  };
+
+  return formatDateTime(format, formats);
+};
+
+// Formats a date and time string based on the provided format string and formats object.
+// format - The format string specifying how date and time should be formatted.
+// formats - An object containing mappings for date and time format tokens.
+// The formatted date and time string according to the format.
+export function formatDateTime(format: string, formats: DateFormat): string {
+  const formattedDateTime: string[] = [];
+
+  format.split(/(\w|.)/m).forEach((type: string | number): void => {
+    if (!type)
+      return;
+
+    if (typeof formats[type as keyof DateFormat] === "undefined")
+      formattedDateTime.push(type.toString());
+    else
+      formattedDateTime.push(formats[type as keyof DateFormat].toString());
+  });
+
+  return formattedDateTime.join("");
+}
+
---
+++ test/strftime.test.ts
@@ -0,0 +1,170 @@
+import { describe, expect, test } from '@jest/globals';
+import { pad, ord, isDateValid, formatDateTime, strftime } from "../src/index";
+import { DateFormat } from '../src/types';
+
+describe('formatDateTime', () => {
+  let formats: DateFormat;
+
+  beforeAll(() => {
+    // Mock DateFormat object
+    formats = {
+      a: 'Mon',
+      A: 'Monday',
+      w: 1,
+      q: '01',
+      d: '10',
+      e: 10,
+      b: 'Jan',
+      B: 'January',
+      m: 1,
+      N: '01',
+      y: '22',
+      Y: 2022,
+      H: 13,
+      h: '13',
+      I: '01',
+      p: 'PM',
+      o: '10th',
+      M: 30,
+      i: '30',
+      S: 45,
+      s: '45',
+      f: 123,
+      c: 'Mon Jan 10 2022 - 13:30:45 GMT',
+      x: '01/10/2022',
+      X: '13:30:45'
+    };
+  });
+
+  test('formats date and time correctly for provided format string', () => {
+    const formatString = 'A, B d, Y => H:M:S';
+    const expectedOutput = 'Monday, January 10, 2022 => 13:30:45';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('handles empty format string', () => {
+    const formatString = '';
+    const expectedOutput = '';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('handles undefined format type', () => {
+    const formatString = 'Z';
+    const expectedOutput = 'Z';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('handles mixed known and unknown format types', () => {
+    const formatString = 'A, b d Z';
+    const expectedOutput = 'Monday, Jan 10 Z';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('formats date and time with consecutive known and unknown format types', () => {
+    const formatString = 'ABdm';
+    const expectedOutput = 'MondayJanuary101';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('handles numeric values correctly', () => {
+    const formatString = 'w m e Y';
+    const expectedOutput = '1 1 10 2022';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+
+  test('handles 12 hour format values correctly', () => {
+    const formatString = 'I:M p';
+    const expectedOutput = '01:30 PM';
+    expect(formatDateTime(formatString, formats)).toBe(expectedOutput);
+  });
+});
+
+describe('strftime', () => {
+  const mockDate = new Date('2018-01-18T21:32:00');
+
+  test('formats correctly using example: new Date().strftime("H:M p - A")', () => {
+    const formatString = 'H:M p - A';
+    const expectedOutput = '21:32 PM - Thursday';
+    expect(strftime(mockDate, formatString)).toBe(expectedOutput);
+  });
+
+  test('formats correctly using example: new Date().strftime("m/b/Y")', () => {
+    const formatString = 'm/b/Y';
+    const expectedOutput = '1/Jan/2018';
+    expect(strftime(mockDate, formatString)).toBe(expectedOutput);
+  });
+
+  test('formats correctly using example: new Date().strftime("do B Y")', () => {
+    const formatString = 'do B Y';
+    const expectedOutput = '18th January 2018';
+    expect(strftime(mockDate, formatString)).toBe(expectedOutput);
+  });
+});
+
+describe('pad', () => {
+  test('pads single digit number to two digits', () => {
+    expect(pad(5)).toBe('05');
+  });
+
+  test('pads double digit number correctly', () => {
+    expect(pad(12)).toBe('12');
+  });
+
+  test('pads single digit number to three digits', () => {
+    expect(pad(5, 3)).toBe('005');
+  });
+});
+
+describe('ord', () => {
+  test('returns correct ordinal suffix for 1', () => {
+    expect(ord(1)).toBe('st');
+  });
+
+  test('returns correct ordinal suffix for 2', () => {
+    expect(ord(2)).toBe('nd');
+  });
+
+  test('returns correct ordinal suffix for 3', () => {
+    expect(ord(3)).toBe('rd');
+  });
+
+  test('returns correct ordinal suffix for 4', () => {
+    expect(ord(4)).toBe('th');
+  });
+
+  test('returns correct ordinal suffix for 11', () => {
+    expect(ord(11)).toBe('th');
+  });
+
+  test('returns correct ordinal suffix for 21', () => {
+    expect(ord(21)).toBe('st');
+  });
+
+  test('returns correct ordinal suffix for 10', () => {
+    expect(ord(10)).toBe('th');
+  });
+
+  test('returns correct ordinal suffix for 42', () => {
+    expect(ord(42)).toBe('nd');
+  });
+
+  test('returns correct ordinal suffix for 53', () => {
+    expect(ord(53)).toBe('rd');
+  });
+});
+
+describe('isDateValid', () => {
+  test('returns true for a valid date', () => {
+    expect(isDateValid(new Date())).toBe(true);
+  });
+
+  test('returns false for an invalid date', () => {
+    expect(isDateValid(new Date('invalid-date'))).toBe(false);
+  });
+
+  test('returns false for non-date objects', () => {
+    expect(isDateValid('string')).toBe(false);
+    expect(isDateValid(123)).toBe(false);
+    expect(isDateValid({})).toBe(false);
+  });
+});
